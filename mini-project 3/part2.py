03b0666d0142d61ad889c9444e120f46a064924f
aidan <aidankeefe486@gmail.com>
2024-04-18 17:49:50 -0600

    did it yippee

--- mini-project 3/part2.py
+++ mini-project 3/part2.py
@@ -1,18 +1,30 @@
 #Please do not change function names and number of parameters.
 #Please check the assignment requirements what function should return.
-
+import numpy as np
+import matplotlib.pyplot as plt
+import random
 
 #TODO: first to get it done
 def k_means_clustering(data_matrix, k, epsilon):
     # Function body left blank
+    iteration = 0
+    clusters = {}
+    mean_points = []
+    range_of_matrix = [sum(data_matrix[:,0]), sum(data_matrix[:,1])]
+    for x in range(k):
+        mean_points.append(random.uniform(range_of_matrix[0], range_of_matrix[1]))
+
+
+
+
     pass
 
-#TODO: Aidan
+#TODO: Tommy
 def dbscan_clustering(data_matrix, minpts, epsilon):
     # Function body left blank
     pass
 
-#TODO: tommy
+#TODO: anyone
 def compute_clustering_precision(true_labels, cluster_labels):
     # Function body left blank
     pass
